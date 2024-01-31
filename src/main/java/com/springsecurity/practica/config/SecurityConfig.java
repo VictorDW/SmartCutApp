88fa0a1ccda9778e48c21244db796f3a4b8f4593
Victor Agudelo <viactur11@gmail.com>
2024-01-31 16:43:16 -0500

    fix(SecurityConfig) se corrigio el nombre del paquete al que pertenece la clase

--- src/main/java/com/springsecurity/practica/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/practica/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.springsecurity.practica.Config;
+package com.springsecurity.practica.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
