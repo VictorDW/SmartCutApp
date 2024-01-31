5d9b6ee86cafd7d6ba5ecc6fa49c570f54d0ee4b
Victor Agudelo <viactur11@gmail.com>
2024-01-31 18:04:09 -0500

    feature(JwtService,application.propperties) se agrego la clave secreta y el tiempo de expiración para la creación del token

--- src/main/java/com/springsecurity/practica/Jwt/JwtService.java
+++ src/main/java/com/springsecurity/practica/Jwt/JwtService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -24,39 +23,38 @@ public class JwtService {
     @Value("${api.security.token.secret}")//asignamos el secret desde application.properties para las buenas prácticas
     private static String SECRET_KEY;
 
+    @Value("${api.security.expiration.minutes}")// se asigna el tiempo de expiración de token
+    private static Integer EXPIRATION_MINUTES;
+
     public String getToken(UserDetails user) {
         return getToken(extraClaims(), user);
     }
 
     private String getToken(Function<User, Map<String, Object>> extraClaims, UserDetails user) {
-        
-        //var key = Jwts.SIG.HS256.key().
 
         return Jwts
         .builder()
         .setClaims(extraClaims.apply((User)user))
         .setSubject(user.getUsername())
         .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis()+10000*60*24))
+        .setExpiration(new Date(System.currentTimeMillis() + (EXPIRATION_MINUTES * 60 * 1000)))
         .signWith(getKey(), SignatureAlgorithm.HS256)
         .compact();
  
     }
 
     private Function<User,Map<String, Object>> extraClaims() {
-
         return user -> {
-            Map<String, Object> extraClaims = new HashMap<>();
-            extraClaims.put("name", user.getFirstName());
-            return extraClaims;
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("Name", user.getFirstName());
+            claims.put("Rol", user.getAuthorities());
+            return claims;
         };
     }
 
     private Key getKey() {
-       
+        // decodifica la clave secreta
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.hmacShaKeyFor(keyBytes); // a su vez se hashea la clave para mayor seguridad
     }
-
-
 }
